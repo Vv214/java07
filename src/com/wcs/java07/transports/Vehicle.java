69b9a6cb1802716e1d8405c9a1a3b8a2b16f1463
Veran <68382853+Vv214@users.noreply.github.com>
2023-07-04 16:39:38 +0200

    Update Vehicle.java

--- src/com/wcs/java07/transports/Vehicle.java
+++ src/com/wcs/java07/transports/Vehicle.java
@@ -24,6 +24,7 @@ abstract class Vehicle {
     public void setKilometers(int kilometers) {
         this.kilometers = kilometers;
     }
-
+    
+    @Override
     public abstract String doStuff();
 }
